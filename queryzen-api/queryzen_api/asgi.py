a2e7ce2b9d2e2b1d3012814ae9a133ef05c098e9
Andrés Garrido <andreslp0001@gmail.com>
2025-01-24 16:18:55 +0100

    Fix asgi env typo

--- queryzen-api/queryzen_api/asgi.py
+++ queryzen-api/queryzen_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for idk project.
+ASGI config for queryzen_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'idk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'queryzen_api.settings')
 
 application = get_asgi_application()
--- queryzen-api/queryzen_api/wsgi.py
+++ queryzen-api/queryzen_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for idk project.
+WSGI config for queryzen-api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
