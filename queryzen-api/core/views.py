206211b02e09b1a2e652638914c3ead31db211f9
surister <surister98@gmail.com>
2025-01-26 16:03:31 +0100

    Add several endpoints:
    1. Rename QuerySerializer to LambdaQuerySerializer
    2. Change GenericViewSet to ModelViewSet
    3. Add retrieve endpoint, GET /lambda/lambda_name returns the

--- queryzen-api/core/views.py
+++ queryzen-api/core/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, serializers, viewsets, status
+
+from rest_framework import mixins, serializers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from core.models import LambdaQuery
+from .models import LambdaQuery
 
 
-class QueryLambdasViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class QueryLambdasViewSet(viewsets.ModelViewSet):
     class ParametrizedSerializer(serializers.Serializer):
         class ParameterSerializer(serializers.Serializer):
             name = serializers.CharField()
             value = serializers.JSONField()
 
-        parameters = ParameterSerializer(many=True)
+        parameters = ParameterSerializer(many=True, allow_null=True)
 
-    class QuerySerializer(serializers.ModelSerializer):
+    class LambdaQuerySerializer(serializers.ModelSerializer):
         class Meta:
             model = LambdaQuery
             fields = '__all__'
@@ -22,15 +23,25 @@ class QueryLambdasViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     def get_serializer_class(self):
         if self.action == 'version':
             return self.ParametrizedSerializer
-        return self.QuerySerializer
+        return self.LambdaQuerySerializer
 
-    serializer_class = QuerySerializer
+    serializer_class = LambdaQuerySerializer
     queryset = LambdaQuery.objects.all()
 
+    # lambdas/{lambda_query}
+    def retrieve(self, request, pk):
+        # TODO: This is what we get, {
+        #     "detail": "No LambdaQuery matches the given query."
+        # }
+        # Change to a more friendly error.
+        obj = get_object_or_404(LambdaQuery, name=pk)
+        serializer = self.LambdaQuerySerializer(obj)
+        return Response(serializer.data)
+
     @action(
-        methods=['POST'],
-        detail=True, url_path='version/(?P<version>[^/.]+)',
-        serializer_class=ParametrizedSerializer
+        methods=['GET'],
+        detail=True,
+        url_path='version/(?P<version>[^/.]+)',
     )
     def version(self, request, pk, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -41,7 +52,7 @@ class QueryLambdasViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         for param in serializer.validated_data['parameters']:
             result_query = result_query.replace(f':{param['name']}', str(param['value']))
 
-        serialized_response = self.QuerySerializer(
+        serialized_response = self.LambdaQuerySerializer(
             {
                 'name': lambda_query.name,
                 'query': result_query,
