9e8eb1afdf5c2b713e7b7e65eedda55fce4ca2b6
Andrés Garrido <andreslp0001@gmail.com>
2025-01-29 19:34:46 +0100

    Refactored and simplified CollectionsViewSet
    Added list collections

--- queryzen-api/apps/core/urls.py
+++ queryzen-api/apps/core/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from apps.core.views import QueryZenViewSet, CollectionsViewSet, ZenApiView
+from apps.core.views import TransversalZenViewSet, CollectionsViewSet
 
 router = DefaultRouter()
 
-router.register('zen', QueryZenViewSet, basename='query-lambdas')
 router.register(
-    r'collections/(?P<collection_name>\w+)/zen/(?P<zen_name>\w+)',
+    'zen', TransversalZenViewSet,
+    basename='zens'
+)
+router.register(
+    r'collections',
     CollectionsViewSet,
     basename='collections'
 )
-urlpatterns = [
-    path(r'collections/<str:collection_name>/zen/<str:zen_name>', ZenApiView.as_view(), name='zen-api'),
-]
-urlpatterns += router.urls
+
+urlpatterns = router.urls
--- queryzen-api/apps/core/views.py
+++ queryzen-api/apps/core/views.py
@@ -1,56 +1,105 @@
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
-from rest_framework.exceptions import MethodNotAllowed, ValidationError
+from rest_framework.exceptions import ValidationError, NotFound, MethodNotAllowed
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from apps.core.filters import QueryZenFilter, ZenFilter
 from apps.core.models import QueryZen
-from apps.core.serializers import QueryZenSerializer, CreateZenSerializer
+from apps.core.serializers import QueryZenSerializer, CreateZenSerializer, DeleteZenSerializer, CollectionsSerializer
 
 from django_filters import rest_framework as filters
 
 from rest_framework import mixins, viewsets, status
 
 
-class QueryZenViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TransversalZenViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = QueryZen.objects.all()
     serializer_class = QueryZenSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = QueryZenFilter
 
 
-class CollectionsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class CollectionsViewSet(viewsets.ModelViewSet):
     queryset = QueryZen.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ZenFilter
 
-    def get_serializer_class(self):
-        if self.action == 'update':
-            return CreateZenSerializer
-        return QueryZenSerializer
+    def _handle_request(self, request, collection_name, *args, **kwargs):
+        if self.request.method == 'GET':
+            return self._get_zens_given(collection_name, kwargs.get('zen_name'))
+        if self.request.method == 'PUT':
+            return self._create_zen(request, collection_name, kwargs.get('zen_name'))
+        if self.request.method == 'DELETE':
+            return self._delete_zen(request, collection_name, kwargs.get('zen_name'))
+        if self.request.method == 'POST':
+            return self._run_zen(request, collection_name, kwargs.get('zen_name'))
+        else:
+            raise MethodNotAllowed({'msg': f'{self.request.method} is not allowed'})
 
-    def list(self, request, *args, **kwargs):
+    def _run_zen(self, request, collection_name, zen_name):
+        return Response({'zen_name': zen_name})
+
+    def _delete_zen(self, request, collection_name, zen_name):
+        serializer = DeleteZenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        if not QueryZen.objects.filter(
+                version=serializer.validated_data['version'],
+                name=zen_name,
+                collection=collection_name
+        ).exists():
+            raise NotFound()
+
+        QueryZen.objects.filter(
+            version=serializer.validated_data['version'],
+            name=zen_name,
+            collection=collection_name
+        ).delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def _get_zens_given(self, collection_name, zen_name):
         queryset = self.filter_queryset(self.get_queryset())
         queryset = queryset.filter(
-            collection=kwargs.get('collection_name'),
-            name=kwargs.get('zen_name'),
+            collection=collection_name,
+            name=zen_name,
         )
         return Response(
             QueryZenSerializer(queryset, many=True).data,
         )
 
-
-class ZenApiView(APIView):
-    def put(self, request, *args, **kwargs):
+    def _create_zen(self, request, collection_name, zen_name):
         serializer = CreateZenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        if QueryZen.objects.filter(name=kwargs.get('zen_name'), version=serializer.validated_data['version']).exists():
+        if QueryZen.objects.filter(
+                name=zen_name,
+                version=serializer.validated_data['version'],
+                collection=collection_name
+        ).exists():
             raise ValidationError(f'This zen already has a version {serializer.validated_data["version"]}')
 
         zen = QueryZen.objects.create(
             **serializer.validated_data,
-            **{'name': kwargs.get('zen_name'), 'collection': kwargs.get('collection_name')}
+            **{'name': zen_name, 'collection': collection_name}
         )
 
         return Response(QueryZenSerializer(zen).data, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        queryset = QueryZen.objects.values('collection').annotate(zen_count=Count('name'))
+        return Response(CollectionsSerializer(queryset, many=True).data)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(
+            collection=pk,
+        )
+        return Response(
+            QueryZenSerializer(queryset, many=True).data,
+        )
+
+    @action(detail=True, methods=['get', 'put', 'delete', 'post'], url_path='zen/(?P<zen_name>[^/.]+)')
+    def zen(self, request, pk, *args, **kwargs):
+        return self._handle_request(request, pk, *args, **kwargs)
