03355f2f9705151ed60f6cc576f53759376487eb
surister <surister98@gmail.com>
2025-01-31 12:51:35 +0100

    Tons of improvements

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
+from apps.core.models import QueryZenfrom apps.core.models import QueryZenfrom apps.core.models import QueryZen
+
 # queryzen
+
+This is the canonical ``queryzen`` client (Python).d
+
+* Features: 100% coverage and thorough testing.
+* Compatability with backend is directly tested and can be enforced at runtime.
+* Pythonic Ask for forgiveness (what were the initials?) and type safe API, checked with mypi.
+* 100% use of docstrings and documentation [link documentation].
+
+# Example
+
+```python
+from querzen import QueryZen
+
+qz = QueryZen()
+
+# Default collection is 'main' and version is 'latest'
+some_zen = qz.create("my_zen", query='select * from sys.summits where country == :country and height > :height')
+print(some_zen)
+
+# Create a Zen that has two parameters, 'country' and 'height', the character ':' is used to specify parameters.
+# if no default parameters are specified, they are m
+zen = qz.create(
+    name="mountains",
+    collection="production",
+    query="select * from sys.summits where country == :country and height > :height",
+    default = {
+        'country': 'United States',
+        'height': 0
+    },
+    version=32,
+    description="Gets all summits from a given country and height, defaults to all summits from United States",
+)
+
+print(zen == qz.get("mountains", collection="production")) # if no version is specified, it returns the created.
+# True
+
+
+# Run with default values.
+result = qz.run(
+    zen
+) # Execution is blocked, it is run synchronously.
+
+print(result.has_data)
+print()
+
+# Run with parameters.
+result = qz.run(
+    zen,
+    country='Spain',
+    height=1200
+)
+
+print(result.as_table())
+print(zen.executions.oldest.as_table())
+
+```
+
+# API
+
+Main classes are QueryZen, Zen and ZenExecutions.
+
+```python
+QueryZen.get
+QueryZen.create
+QueryZen.get_or_create
+QueryZen.delete
+QueryZen.list
+```
+
+
--- queryzen-client/queryzen/queryzen.py
+++ queryzen-client/queryzen/queryzen.py
@@ -6,11 +6,22 @@ from queryzen.backend import QueryZenHttpClient, QueryZenClientABC
 from queryzen.exceptions import ZenAlreadyExists, UnknownError, ZenDoesNotExist
 from queryzen.types import AUTO
 
+# The default collection of queryzen is 'main', if not specified
+# we will always query this. # Fixme unit test this.
+DEFAULT_COLLECTION = 'main'
 
+@dataclasses.dataclass
 class ZenExecution:
     """
     Represents the execution of a Zen in a Zen backend.
     """
+    rows: Rows
+    columns: Columns
+    row_count: int
+
+    def has_data(self):
+        return self.row_count > 0
+
 
 
 @dataclasses.dataclass
@@ -21,13 +32,13 @@ class Zen:
     query: str
     description: str
     created_at: datetime.datetime
-    collection: str = dataclasses.field(default_factory=lambda: "main")
+    collection: str = dataclasses.field(default_factory=lambda: DEFAULT_COLLECTION)
     created_by: str = dataclasses.field(default_factory=lambda: "not_implemented")
     state: typing.Literal['valid', 'invalid', 'unknown'] = dataclasses.field(
         default_factory=lambda: 'unknown')
     executions: list = dataclasses.field(default_factory=list)  # Improve typing
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return dataclasses.asdict(self)
 
 
@@ -36,14 +47,14 @@ class QueryZen:
             self,
             client: QueryZenClientABC | None = None
     ):
-        self._client = client or QueryZenHttpClient()
+        self._client: QueryZenClientABC = client or QueryZenHttpClient()
 
     def create(
             self,
             name: str,
             query: str,
             description: str = None,
-            collection: str = 'main',
+            collection: str = DEFAULT_COLLECTION,
             version=AUTO
     ):
         response = self._client.create(
@@ -57,33 +68,125 @@ class QueryZen:
         if response.error:
             if response.error_code == 409:
                 raise ZenAlreadyExists(response.error)
+
             raise UnknownError(response.error)
+
+        if not response.data:
+            raise UnknownError('When creating a Zen, the JSON representation of the object was not returned') # Fixme improve this
 
         return Zen(**response.data[0])
 
-    def get(self, name: str, version=AUTO) -> Zen | None:
+    def get(self, name: str, collection=DEFAULT_COLLECTION, version=AUTO) -> Zen:
+        """
+        Get one zen from the given name, collection and version, raises ZenDoesNotExist
+        if it does not exist.
+
+        If you don't want to handle if the query does not exist and just want a `Zen`,
+        see `QueryZen.get_or_create`.
+
+        Args:
+            name: The name of the `Zen`
+            collection: The collection of the `Zen`, defaults to ``DEFAULT_COLLECTION``.
+            version: The version of the `Zen`, defaults to 'AUTO'.
+
+        Example:
+            qz = QueryZen()
+
+            try:
+                zen = qz.get(name='myzen', version=23)
+
+            except ZenDoesNotExist:
+                handle_zen_not_existing()
+
+        Raises:
+            `ZenDoesNotExist` if the `Zen` does not exist.
+
+        Returns:
+            A zen, if it exists.
+        """
         response = self._client.get_one(
             name=name,
-            version=version
+            version=version,
+            collection=collection
         )
 
         if not response.data:
             raise ZenDoesNotExist()
 
-        if not response.error:
-            return Zen(**response.data[0])
+        if response.error:
+            raise UnknownError(response.error, status_code=response.error_code)
+
+        return Zen(**response.data[0])
 
     def list(self, **filters) -> list[Zen]:
         """
-        Lists all ``Zen``, accepts additional filters like 'name', 'collection', 'version' # todo add execution filter
-        :return:
+        Lists all ``Zen``, accepts additional filters like `name`, `collection`, `version` # todo add execution filter
+
+        This method uses a special filtering endpoint that is different from the REST endpoints we
+        use, it offers special filtering options like filtering by execution sub parameters,
+        see examples for use cases.
+
+        It does not raise Exceptions, if you want to be extra safe, use QueryZen.get or QueryZen.get_or_create
+
+        Args:
+            filters: The filters that will be used.
+
+        Examples:
+
+            ```
+            qz = QueryZen()
+
+            zens = qz.list(name='mountain', executions__status='VALID')
+            if zens:
+                do_something_with_zens(zens)
+            # Will return all ``Zen``s with name 'mountain' whose execution status is valid # todo check wording on this
+
+            ```
+
+        Returns:
+             A list of ``Zen``, empty if none is found.
         """
         response = self._client.get_all(**filters)
-        if not response.error:
-            return [
+
+        if response.error:
+            raise UnknownError(response.error, status_code=response.error_code)
+
+        return [
                 Zen(**data) for data in response.data
             ]
 
+    def get_or_create(
+            self,
+            name: str,
+            query: str,
+            collection: str = DEFAULT_COLLECTION,
+            version=AUTO
+    ) -> (bool, Zen):
+        """
+        Get a Zen or create it.
+
+
+        Examples:
+            >>> created, zen = QueryZen().get_or_create(name='myzen', query='select * from t where h > :value')
+            False, Zen(name='myzen', version='latest', ...)
+
+            >>> _, zen = QueryZen().get_or_create(name='z', query='q', version=34)
+            True, Zen(name='z', ...)
+
+        Args:
+            name: The name of the Zen.
+            query: The query (only used if created).
+            collection: The collection of the Zen (defaults to ``DEFAULT_COLLECTION``).
+            version: The version of the Zen (defaults to 'AUTO', the latest will be returned if it exists).
+
+        Returns:
+            (created, Zen) Returns a tuple, the first element is a bool on whether the zen was
+            created or not, the second element is the Zen.
+        """
+        try:
+            return False, self.get(name=name, collection=collection, version=version)
+        except ZenDoesNotExist:
+            return True, self.create(name=name, collection=collection, version=version, query=query)
+
     def run(self, **params) -> None:
         raise NotImplementedError
-
