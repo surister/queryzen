67ed449cb3c30a742e6bb9ad1f4667154583ec6d
surister <surister98@gmail.com>
2025-02-01 14:51:00 +0100

    Fix lint errors

--- queryzen-client/queryzen/constants.py
+++ queryzen-client/queryzen/constants.py
@@ -1,3 +1,7 @@
+"""
+Constants for QueryZen.
+"""
+
 # The default collection of queryzen is 'main', if not specified
 # we will always query this. # Fixme unit test this.
 DEFAULT_COLLECTION = 'main'
