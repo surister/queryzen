f41d7d0b8ebe95e7b96d2359dfa8af0b96f88894
surister <surister98@gmail.com>
2025-02-02 14:22:04 +0100

    Rename UnknownError to UncaughtBackendError and improve its message

--- queryzen-client/queryzen/exceptions.py
+++ queryzen-client/queryzen/exceptions.py
@@ -1,5 +1,7 @@
 # pylint: disable=C0114
 
+from queryzen.backend import QueryZenResponse
+
 class ZenAlreadyExists(Exception):
     """
     Raised if you try to create a ``Zen`` that already exists.
@@ -21,40 +23,29 @@ class IncompatibleAPI(Exception):
     # Todo add message
     pass
 
-class UnknownError(Exception):
+class UncaughtBackendError(Exception):
     """
     Raised when the backend returns an error code that we do not implicitly catch.
     """
-    # TODO (ivan), rename UnknownError to UncaughtBackendError. Make it beautifully print
-    # the response in the console so if it ever happens, we get a faster Q/A + bugfix.
 
-    # Ideally, we receive one 'context', string where we the developer explain the context
-    # from which this was thrown, e.g. UnknownError("When creating a Zen, the JSON representation
-    # of the object was not returned", ...) Structure of the raised error:
 
-    # Exception(
-    #     """
-    #     Error from backend not caught gracefully, this is a bug, please report this entire
-    #     message in a Github Issue:
-    #
-    #     https://github.com/surister/queryzen/issues/new?template=Blank+issue
-    #
-    #     Response from backend:
-    #
-    #     Response(
-    #         status_code = 200,
-    #         error_message = "error message",
-    #     )
-    #
-    #     Involved Zen (if Any)
-    #
-    #     Zen(
-    #         name='zen',
-    #         ...
-    #     )
-    #     """
-    # )
-    def __init__(self, message: str, status_code: int):
-        self.message = (f'Error from backend not caught gracefully, this is a bug,'
-                        f' please report it: {status_code} {message}')
+    def __init__(self, response: QueryZenResponse, zen: 'Zen' = None, context: str = None):
+        self.message = f"""
+        Error from backend not caught gracefully, this is a bug, please report this entire
+        message in a Github Issue:
+
+        https://github.com/surister/queryzen/issues/new?template=Blank+issue
+
+        Response from backend:
+        --
+        {response!r}
+
+        Involved Zen (if any):
+        --
+        {zen}
+        
+        Extra context:
+        --
+        {context}
+        """
         super().__init__(self.message)
